905052fc9842f893f44a72e6af893c09de934bb9
chinguun101 <100122425+chinguun101@users.noreply.github.com>
2024-09-08 17:27:55 +0200

    [40] [Time Beats: 5.07%] [Memory Beats: 68.42%] - LeetPush

---
+++ 40-Combination-Sum-II.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res=[]
+        comb=[]
+        candidates=sorted(candidates)
+        def helper(i):
+            if sum(comb)==target:
+                res.append(list(comb))
+                return
+            elif i>len(candidates)-1 or sum(comb)>target:
+                return
+            
+            comb.append(candidates[i])
+            helper(i+1)
+            comb.pop()
+            while i<len(candidates)-1 and candidates[i]==candidates[i+1]:
+                i+=1
+            helper(i+1)
+
+        helper(0)
+        return res
