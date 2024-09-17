9f17919e1ab230fe2dba8f3c33f725eabe7c5453
chinguun101 <100122425+chinguun101@users.noreply.github.com>
2024-09-17 19:05:42 +0200

    [416] [Time Beats: 80.92%] [Memory Beats: 48.77%] - LeetPush

---
+++ 416-Partition-Equal-Subset-Sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if sum(nums)%2:
+            return False
+        target=sum(nums)//2
+        dp=set()
+        dp.add(0)
+        for i in range(len(nums)-1,-1,-1):
+            nextDP=set()
+            for t in dp:
+                if (t+nums[i])==target:
+                    return True
+                nextDP.add(t+nums[i])
+                nextDP.add(t)
+            dp=nextDP
+        return False
