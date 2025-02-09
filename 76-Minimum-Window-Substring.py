209d294fad09796bbe943fab4e7c6c89436023fe
Chinguun Ganbaatar <100122425+chinguun101@users.noreply.github.com>
2025-02-08 17:07:08 -0800

    [76] [Time Beats: 13.43%] [Memory Beats: 33.80%] - LeetPush

--- 76-Minimum-Window-Substring.py
+++ 76-Minimum-Window-Substring.py
@@ -5,35 +5,30 @@ class Solution(object):
         :type t: str
         :rtype: str
         \\\
-        if t==\\:
-            return \\
+        window = [0]*58
+        res = \\
+        resLen = float('inf')
+        for letter in t:
+            window[ord(letter) - ord('A')]+=1
+        print(window)
 
-        count, window={},{}
-        for i in t:
-            count[i]=1+count.get(i,0)
-        
-        res, reslen = [-1, -1], float(\infinity\)
-        
-        have,need= 0,len(count)
-        l=0
+        l = 0
+        def check(window):
+            for c in window:
+                if c > 0:
+                    return False
+            return True
+
         for r in range(len(s)):
-            c=s[r]
-            window[c]=1+window.get(c,0)
-            if c in count and count[c]==window[c]:
-                have+=1
-
-            while have==need:
-                if (r - l + 1) < reslen:
-                    res=[l,r]
-                    reslen=r-l+1
-
-                window[s[l]]-=1
-                
-                if s[l] in count and window[s[l]]<count[s[l]]:
-                    have-=1
+            window[ord(s[r]) - ord('A')]-=1
+            while check(window):
+                if resLen > r-l+1:
+                    resLen = r-l+1
+                    res = s[l:r+1]
+                window[ord(s[l]) - ord('A')]+=1
                 l+=1
-        l,r= res
-        return s[l : r + 1] if reslen != float(\infinity\) else \\
 
-        
+        return res
+
+
         
